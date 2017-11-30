74fe6e07b1fdaabdb855615dc0673ae95d5734d2
mayoorpatil <mayoor.patil@libertymutual.com>
2017-11-30 15:20:32 -0500

    Serialize status column on the orders table

--- app/serializers/order_serializer.rb
+++ app/serializers/order_serializer.rb
@@ -1,5 +1,5 @@
 class OrderSerializer < ActiveModel::Serializer
-  attributes :id, :name, :address, :email, :phone_number, :amount
+  attributes :id, :name, :address, :email, :phone_number, :amount, :status
   has_many :assignment
   has_many :products, through: :assignment
 end
